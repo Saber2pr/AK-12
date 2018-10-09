e2db9a82fc01e1ae9d99ab2b76d97d304863b29e
saber2pr <saber2pr@gmail.com>
2018-10-09 17:20:24 +0800

    update

--- data/home.js
+++ data/home.js
@@ -1,16 +1,17 @@
-{
-  "title": "AK-12's Website",
-  "logo": "https://github.com/Saber2pr/MyWeb/raw/master/resource/AK12.jpg",
-  "author": "I am AK-12",
-  "readME": "the Website is based on Vue",
-  "websites": [{
-    "name": "github",
-    "href": "https://github.com/Saber2pr"
+/* eslint-disable */
+const config = {
+  'title': "AK-12's Website",
+  'logo': 'https://github.com/Saber2pr/MyWeb/raw/master/resource/AK12.jpg',
+  'author': 'I am AK-12',
+  'readME': 'the Website is based on Vue',
+  'websites': [{
+    'name': 'github',
+    'href': 'https://github.com/Saber2pr'
   }, {
-    "name": "csdn",
-    "href": "https://blog.csdn.net/u011607490"
+    'name': 'csdn',
+    'href': 'https://blog.csdn.net/u011607490'
   }, {
-    "name": "bilibili",
-    "href": "https://space.bilibili.com/71959910/#/"
+    'name': 'bilibili',
+    'href': 'https://space.bilibili.com/71959910/#/'
   }]
 }
